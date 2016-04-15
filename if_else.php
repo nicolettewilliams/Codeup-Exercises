5b52c012c1ddb57b7f08f35112f82a587460a47c
nicolette <nicolette.williams8@icloud.com>
2016-04-15 10:24:35 -0500

    finished exercise

--- if_else.php
+++ if_else.php
@@ -18,18 +18,16 @@ if ($b < $c) {
     echo "$b is less than $c\n";
 } else {
     echo "$b is greater than or equal to $c\n";
-}
+};
 
-// // TODO:
-// // combine the next 3 conditionals into one
-// // if/elseif/else block that checks in order for:
-// // identical, equal, not equal/identical
-// if ($b === $c) {
-//     echo "$b is identical to $c\n";
-// }
-// if ($b == $c) {
-//     echo "$b is equal to $c\n";
-// }
-// if ($b != $c) {
-//     echo "$b is not equal to $c\n";
-// }
+// TODO:
+// combine the next 3 conditionals into one
+// if/elseif/else block that checks in order for:
+// identical, equal, not equal/identical
+if ($b === $c) {
+    echo "$b is identical to $c\n";
+} elseif ($b == $c) {
+    echo "$b is equal to $c\n";
+} else {
+    echo "$b is not equal to $c\n";
+};
