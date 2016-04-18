4ddf921b3a7225008f6ead00e817f22f6a69b642
nicolette <nicolette.williams8@icloud.com>
2016-04-18 10:23:49 -0500

    makes sure the user enters a number

--- for.php
+++ for.php
@@ -1,15 +1,25 @@
 <?php
-fwrite(STDOUT, 'Starting number: ');
-$startNumber = (int)fgets(STDIN);
-fwrite(STDOUT, 'Ending number: ');
-$endNumber = (int)fgets(STDIN);
+
+$firstTry = true;
+do{
+    if(!$firstTry){
+        fwrite(STDOUT, 'Use numbers.' . PHP_EOL);
+    }
+
+    fwrite(STDOUT, 'Starting number: ');
+    $startNumber = trim(fgets(STDIN));
+    fwrite(STDOUT, 'Ending number: ');
+    $endNumber = trim(fgets(STDIN));
+    $firstTry = false;
+
+}while(!is_numeric($startNumber) || !is_numeric($endNumber));
+
 fwrite(STDOUT, 'Increment by: ');
 $increment = (int)fgets(STDIN);
 
 if($increment == 0){
     $increment = 1;
 }
-
-for($i = $startNumber; $i <= $endNumber; $i += $increment){
+for($i = (int)$startNumber; $i <= (int)$endNumber; $i += $increment){
     echo $i . PHP_EOL;
-}
+};
