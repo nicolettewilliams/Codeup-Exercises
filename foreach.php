cc26dff8341cf61869bd60af5aa4eabae42cc7b6
nicolette <nicolette.williams8@icloud.com>
2016-04-18 13:42:11 -0500

    loop that echos eout every value

--- foreach.php
+++ foreach.php
@@ -1,6 +1,15 @@
 <?php
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
 foreach($things as $thing){
-    echo is_scalar($thing) . PHP_EOL;
-}
+    if(is_array($thing)){
+        echo "array[ ";
+        foreach($thing as $value){
+            echo "$value ";
+        }
+        echo "]" . PHP_EOL;
+    }else{
+        echo $thing . PHP_EOL;
+    };
+};
