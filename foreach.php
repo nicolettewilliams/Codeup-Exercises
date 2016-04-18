81dd3fb431c6613a3213bb5d881c147898f7b5c8
nicolette <nicolette.williams8@icloud.com>
2016-04-18 13:39:01 -0500

    loop taht only outputs scalar values and leaves blanks for the rest

--- foreach.php
+++ foreach.php
@@ -2,5 +2,5 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 foreach($things as $thing){
-    echo gettype($thing) . PHP_EOL;
-};
+    echo is_scalar($thing) . PHP_EOL;
+}
