2629ce290ecb229112fd0576a9d23ce11f82a4df
nicolette <nicolette.williams8@icloud.com>
2016-04-19 14:07:09 -0500

    functions

--- arithmetic.php
+++ arithmetic.php
@@ -3,29 +3,50 @@
 $a = 25;
 $b = 5;
 
-function add($a, $b) {
-    return $a + $b . PHP_EOL;
+function add($a, $b){
+    if (numberCheck($a, $b)){
+        return $a + $b;
+    };
+};  
+
+function subtract($a, $b){
+    if (numberCheck($a, $b)){
+        return $a - $b;
+    };
 };
 
-function subtract($a, $b) {
-    return $a - $b . PHP_EOL;
+function multiply($a, $b){
+    if (numberCheck($a, $b)){
+        return $a * $b;
+    };
 };
 
-function multiply($a, $b) {
-    return $a * $b . PHP_EOL;
+function divide($a, $b){
+    if(!$b){
+        return "ERROR: Can not divide by 0. Undefined.";
+    }else if (numberCheck($a, $b)){
+        return $a / $b;
+    };
 };
 
-function divide($a, $b) {
-    return $a / $b . PHP_EOL;
+function modulus($a, $b){
+    if (numberCheck($a, $b)){
+        return $a % $b;
+    };   
 };
 
-function modulus($a, $b) {
-    return $a % $b . PHP_EOL;
+function numberCheck($a, $b){
+    if (is_numeric($a) && is_numeric($b)){
+        return true;
+    }else{
+        echo "ERROR: Make sure inputs $a and $b are both numbers.";
+        return false;
+    };
 };
 
-echo add($a , $b);
-echo subtract($a , $b);
-echo multiply($a , $b);
-echo divide($a , $b);
-echo modulus($a, $b);
+echo add($b, 2) . PHP_EOL;
+echo subtract($a, 2) . PHP_EOL;
+echo multiply($a, $b) . PHP_EOL;
+echo divide($a, 0) . PHP_EOL;
+echo modulus(6, 3) . PHP_EOL;
 
