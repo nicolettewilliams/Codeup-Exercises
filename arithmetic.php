182492ccdfb47749d5fe3109346adc6dda57a52c
nicolette <nicolette.williams8@icloud.com>
2016-04-19 12:53:01 -0500

    added modulus

--- arithmetic.php
+++ arithmetic.php
@@ -16,8 +16,13 @@ function divide($a, $b)
 {
     return $a / $b;
 }
+function modulus($a, $b)
+{
+    return $a % $b;
+}
 
 echo add(4, 2) . PHP_EOL;
 echo subtract(4, 2) . PHP_EOL;
 echo multiply(4, 2) . PHP_EOL;
 echo divide(4, 2) . PHP_EOL;
+echo modulus(4, 2) . PHP_EOL;
