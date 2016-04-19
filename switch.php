107ea0af0f2d53232291db501325b5a9397f47fe
nicolette <nicolette.williams8@icloud.com>
2016-04-19 11:26:55 -0500

    getting day of the week using a switch statement

---
+++ switch.php
@@ -0,0 +1,35 @@
+<?php 
+
+// Set the default timezone
+date_default_timezone_set('America/Chicago');
+
+// Get Day of Week as number
+// 1 (for Monday) through 7 (for Sunday)
+$dayOfWeek = date('N');
+
+switch($dayOfWeek) {
+    case 1:
+        echo "Monday" . PHP_EOL;
+        break;
+    case 2:
+        echo "Tuesday" . PHP_EOL;
+        break;
+    case 3: 
+        echo "Wednesday" . PHP_EOL;
+        break;
+    case 4:
+        echo "Thursday" . PHP_EOL;
+        break;
+    case 5:
+        echo "Friday" . PHP_EOL;
+        break;
+    case 6:
+        echo "Saturday" . PHP_EOL;
+        break;
+    case 7:
+        echo "Sunday" . PHP_EOL;
+        break; 
+    default:
+        echo "Expected value of 1-7" . PHP_EOL; 
+        break;       
+ };
